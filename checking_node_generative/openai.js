aba7583ae4b024455550ef64e859448f9e4976d5
Michal Feldman <michal@apiiro.com>
2023-11-30 17:13:17 +0200

    JS

--- checking_node_generative/openai.js
+++ checking_node_generative/openai.js
@@ -1,4 +1,4 @@
-const { Configuration, OpenAIApi } = require("openai");
+import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
   apiKey: "your_api_key",
 });
