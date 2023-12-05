3295fd15f83d41ec0bf74c40df4c433fbb207afb
Michal Feldman <michal@apiiro.com>
2023-12-05 10:17:03 +0200

    added new files bouncyCastle dotnet

---
+++ Encrpytion/Csharp/bouncyCastle.cs
@@ -0,0 +1,19 @@
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+public class ECKeyPairGenerationExample
+{
+    public static AsymmetricCipherKeyPair GenerateECKeyPair()
+    {
+        // Assume you have EC domain parameters already set up (not shown here)
+        ECDomainParameters ecDomainParameters = new ECDomainParameters(/* ... */);
+
+        var keyGenerationParameters = new ECKeyGenerationParameters(ecDomainParameters, new SecureRandom());
+        var keyPairGenerator = GeneratorUtilities.GetKeyPairGenerator("EC");
+        keyPairGenerator.Init(keyGenerationParameters);
+
+        return keyPairGenerator.GenerateKeyPair();
+    }
+}
