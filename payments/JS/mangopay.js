578f7b46d146f6bc04d0592914fbb094b06a37ff
Michal Feldman <michal@apiiro.com>
2023-12-13 15:51:31 +0200

    stripe and mangopay

--- payments/JS/mangopay.js
+++ payments/JS/mangopay.js
@@ -1,10 +1,13 @@
 var mangopay = require('mangopay2-nodejs-sdk');
 
-var api = new mangopay({
-    clientId: 'your_client_id',
-    clientApiKey: 'your_client_api_key',
-    // Set the right production API url. If testing, omit the property since it defaults to sandbox URL
-    baseUrl: 'https://api.mangopay.com'
-});
 
-api.Users.create()
+async function main(){
+    var api = new mangopay({
+        clientId: 'your_client_id',
+        clientApiKey: 'your_client_api_key',
+        // Set the right production API url. If testing, omit the property since it defaults to sandbox URL
+        baseUrl: 'https://api.mangopay.com'
+    });
+
+    api.Users.create()
+}
