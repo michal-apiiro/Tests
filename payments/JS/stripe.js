ca47190bafd901649ea7fe392889f5d52b312b05
Michal Feldman <michal@apiiro.com>
2023-12-17 09:36:08 +0200

    stripe

--- payments/JS/stripe.js
+++ payments/JS/stripe.js
@@ -3,6 +3,7 @@ const app = express();
 const { resolve } = require('path');
 // Copy the .env.example in the root into a .env file in this folder
 require('dotenv').config({ path: './.env' });
+const stripe = require('stripe');
 
 // Ensure environment variables are set.
 checkEnv();
@@ -12,3 +13,5 @@ async function main(){
   const price =  stripe.prices.retrieve(process.env.PRICE);
   const session =  stripe.checkout.sessions.retrieve(sessionId);
 }
+
+
