df05ce43be4d7eed108936e62ebc48d46bb09349
Michal Feldman <michal@apiiro.com>
2023-12-21 14:59:47 +0200

    python added mango files

---
+++ payments/mango3.py
@@ -0,0 +1,16 @@
+from mangopaysdk import MangoPayApi,Wallet
+
+def mangoPay():
+    api = MangoPayApi()
+    api.Config.ClientID = 'your_client_id'
+    api.Config.ClientPassword = 'your_client_password'
+
+    user = users.Get(1)  # assuming that `users` is part of the imported module
+
+    wallet = Wallet()
+    wallet.Owners = [user.Id]
+    wallet.Currency = 'EUR'
+    wallet.Description = 'WALLET IN EUR'
+
+    saved_wallet = wallets.Create(wallet)  # assuming that `wallets` is part of the imported module
+    return saved_wallet
