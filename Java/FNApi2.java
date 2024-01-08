075ffbeca5681ed7136e30f53e850499115cb4f9
Michal Feldman <michal@apiiro.com>
2024-01-08 11:48:11 +0200

    FN api java

--- Java/FNApi2.java
+++ Java/FNApi2.java
@@ -14,17 +14,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @PostMapping(path = "/SpoofCookie/login")
-  @ResponseBody
-  @ExceptionHandler(UnsatisfiedServletRequestParameterException.class)
-  public class login(
-      @RequestParam String username,
-      @RequestParam String password,
-      @CookieValue(value = COOKIE_NAME, required = false) String cookieValue,
-      HttpServletResponse response) {
+@ResponseBody
+@ExceptionHandler(UnsatisfiedServletRequestParameterException.class)
+public class login(
+    @RequestParam String username,
+    @RequestParam String password,
+    @CookieValue(value = COOKIE_NAME, required = false) String cookieValue,
+    HttpServletResponse response) {
 
     if (StringUtils.isEmpty(cookieValue)) {
-      return credentialsLoginFlow(username, password, response);
+        return credentialsLoginFlow(username, password, response);
     } else {
-      return cookieLoginFlow(cookieValue);
+        return cookieLoginFlow(cookieValue);
     }
   }
