ec0a7c4512cc400a9640689087cf28a3d0f5e032
Michal Feldman <michal@apiiro.com>
2024-01-14 12:54:53 +0200

    project goatweb java

---
+++ goatweb-example/src/test/java/com/example/goatwebexample/GoatwebExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goatwebexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoatwebExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
