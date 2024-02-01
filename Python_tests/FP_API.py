69b7027418fad49abd6c18fad69b3c000d0e6350
Michal Feldman <michal@apiiro.com>
2024-02-01 10:28:41 +0200

    api

--- Python_tests/FP_API.py
+++ Python_tests/FP_API.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('path1/', admin.site.urls),
-    path("path2/", include("namespace1.urls", namespace="namespace1")),
-    path("path3/", include("namespace2.urls", namespace="namespace2")),
+    path('path2/', include("namespace1.urls", namespace="namespace1")),
+    path('path3/', include("namespace2.urls", namespace="namespace2")),
     path(f"path4/{settings.CHECK_SECRET_TOKEN}/", include("blabla.urls")),
 ]
