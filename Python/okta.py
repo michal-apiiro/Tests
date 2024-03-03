375ac613915a0bc280fd1197a74d9cbb22aae851
Michal Feldman <michal@apiiro.com>
2024-03-03 09:32:35 +0200

    python okta

--- Python/okta.py
+++ Python/okta.py
@@ -1,7 +1,7 @@
 import requests
 from requests.auth import HTTPBasicAuth
 from okta.client import Client as OktaClient
-
+from okta import UsersClient
 
 def get_okta_user_list(_env_type):
     secrets = get_secret("dremio-utils")
@@ -10,3 +10,16 @@ def get_okta_user_list(_env_type):
     okta_client = OktaClient(okta_config)
     return get_okta_users(okta_client)
 
+
+
+# Initialize the UsersClient with your Okta domain and API token
+users_client = UsersClient("https://{yourOktaDomain}", "{yourApiToken}")
+
+# Retrieve the user by username or user ID
+user = users_client.get_user("user@example.com")
+
+# Check user attributes to determine authorization
+if user.profile.department == "IT" and user.profile.title == "Manager":
+    print("User is authorized for privileged access")
+else:
+    print("User is not authorized for privileged access")
