d51ab3cf13d121df5c3b23aa78ce1b0e30173cea
Michal Feldman <michal@apiiro.com>
2024-04-09 18:18:41 +0300

    java example

---
+++ Java_proj/src/main/java/example_proj/Controller.java
@@ -0,0 +1,14 @@
+package example_proj;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+    @GetMapping
+    public int getDescriptions() {
+        return 0; // Replace null with appropriate logic
+    }
+}
