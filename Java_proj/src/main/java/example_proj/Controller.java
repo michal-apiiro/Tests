aa50fae9a58bec1ef99292a126bc692e1d60e050
Michal Feldman <michal@apiiro.com>
2024-04-10 15:19:03 +0300

    java example1

--- Java_proj/src/main/java/example_proj/Controller.java
+++ Java_proj/src/main/java/example_proj/Controller.java
@@ -1,5 +1,6 @@
 package example_proj;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
